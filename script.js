24b5a51ad3d743dd6c9fdeac72dcb18de9ca8f94
Takumi Kawaguchi <tcgds4@gmail.com>
2021-12-31 19:54:20 +0900

    [update]first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var vm = new Vue({
+    el: '#merge-number',
+    data: {
+        radix: 2,
+        field: [
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]
+        ]
+    },
+    methods: {
+        initFields: function () {
+            
+        }
+    }
+})
